4e9bcb97b28f8f3b63b341b7800dc2b886bbec5e
baharkose <baharkse17@gmail.com>
2023-12-20 22:12:37 +0300

    fixed projects

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -5,28 +5,33 @@ import project3 from "../assets/img/project-3.jpg";
 import project4 from "../assets/img/project-4.jpg";
 import project5 from "../assets/img/project-5.jpg";
 import project6 from "../assets/img/project-6.jpg";
+import "../pages/projects/Projects.scss";
 const ProjectCards = () => {
   return (
-    <div>
-      <div classname="projects__item">
-        <img src={project1} alt="My Project" />
+    <>
+    <main className="projects">
+      <div className="projects__items">
+        <div className="projects__item">
+          <img src={project1} alt="My Project" />
+        </div>
+        <div className="projects__item">
+          <img src={project2} alt="My Project" />
+        </div>
+        <div className="projects__item">
+          <img src={project3} alt="My Project" />
+        </div>
+        <div className="projects__item">
+          <img src={project4} alt="My Project" />
+        </div>
+        <div className="projects__item">
+          <img src={project5} alt="My Project" />
+        </div>
+        <div className="projects__item">
+          <img src={project6} alt="My Project" />
+        </div>
       </div>
-      <div classname="projects__item">
-        <img src={project2} alt="My Project" />
-      </div>
-      <div classname="projects__item">
-        <img src={project3} alt="My Project" />
-      </div>
-      <div classname="projects__item">
-        <img src={project4} alt="My Project" />
-      </div>
-      <div classname="projects__item">
-        <img src={project5} alt="My Project" />
-      </div>
-      <div classname="projects__item">
-        <img src={project6} alt="My Project" />
-      </div>
-    </div>
+      </main>
+    </>
   );
 };
 
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 import "./Projects.scss";
+import bg from "../../assets/img/model-1.jpg";
 
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
+import ProjectCards from "../../components/ProjectCards";
 
 const Projects = () => {
   return (
     <>
-      <div classname="project-bgImg-container">
-        {"{"}/* PROJECT NAV */{"}"}
-       <Navbar/>
-        <div classname="projects__bio-image">
+      <div className="project-bgImg-container">
+        <Navbar />
+        <div className="projects__bio-image">
           <h1>My Projects</h1>
         </div>
+
+        <ProjectCards />
+
+        <footer className="vertical">
+          <Footer />
+        </footer>
       </div>
-      {"{"}/* PROJECT MAIN */{"}"}
-      <main classname="projects">
-        <div classname="projects__items">
-          
-        </div>
-      </main>
-    
-      <footer classname="vertical">
-       <Footer/>
-      </footer>
-     
     </>
   );
 };
