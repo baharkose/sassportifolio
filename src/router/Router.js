f28abeb4fe87b32e071e6f22b431b3798ff94614
baharkose <baharkse17@gmail.com>
2023-12-20 07:45:46 +0300

    init repo

---
+++ src/router/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "../pages/about/About.jsx";
+import Home from "../pages/home/Home.jsx";
+import Contact from "../pages/contact/Contact.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+
+const Router = () => {
+  return (
+    <div>
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Router;
